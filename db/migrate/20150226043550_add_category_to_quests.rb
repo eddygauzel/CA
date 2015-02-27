33e6bd37bb464fe68f7c7590dd3fa11f71b5dc5d
eddygauzel <eddy-gauzel@hotmail.com>
2015-02-27 16:04:56 +0100

    db probleme

--- db/migrate/20150226043550_add_category_to_quests.rb
+++
@@ -1,5 +0,0 @@
-class AddCategoryToQuests < ActiveRecord::Migration
-  def change
-    add_column :quests, :category, :categories
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -28,7 +28,7 @@ ActiveRecord::Schema.define(version: 20150226060858) do
   add_index "active_admin_comments", ["namespace"], name: "index_active_admin_comments_on_namespace"
   add_index "active_admin_comments", ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
 
-  create_table "categories", force: :cascade do |t|
+  create_table "category", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
